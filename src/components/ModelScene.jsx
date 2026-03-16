06b7291b22be2a6a9920a443dea64516b8cd28d1
Sathya Sai <sathyasai410@gmail.com>
2026-03-16 21:02:13 +0530

    fix duplicate merge code

--- src/components/ModelScene.jsx
+++ src/components/ModelScene.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Model from "./model.jsx";
@@ -16,23 +16,5 @@ export default function ModelScene() {
        />
     </Canvas>
   );
-=======
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import Model from "./model.jsx";
 
-export default function ModelScene() {
-  return (
-    <Canvas camera={{ position: [0, 1, 1] }}>
-      <ambientLight intensity={50} />
-      <directionalLight position={[2,2,2]} />
-
-      <Model />
-
-      <OrbitControls enableZoom={false}
-      enablePan={false}
-       />
-    </Canvas>
-  );
->>>>>>> f28f9da7251204656258f8394977c7a1fc1d019a
 }
