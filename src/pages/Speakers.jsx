c11a425bdb734a957390eb5e1ce5366e0f3b4f2c
Sathya Sai <sathyasai410@gmail.com>
2026-03-16 21:15:46 +0530

    https://github.com/Sathya577/esummit-26-ecellbvritn

--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import RippleGrid from "../components/RippleGrid";
 import "./Speakers.css";
 
@@ -16,23 +16,6 @@ function About() {
   );
 }
 
-=======
-import RippleGrid from "../components/RippleGrid";
-import "./Speakers.css";
 
-function About() {
-  return (
-    <div className="about-wrapper">
 
-      
-      <div className="coming-soon">
-        <h1>Coming Soon</h1>
-        <p>Revealing speakers for esummit soon</p>
-      </div>
-
-    </div>
-  );
-}
-
->>>>>>> f28f9da7251204656258f8394977c7a1fc1d019a
 export default About;
