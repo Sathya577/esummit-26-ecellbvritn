08144b4b38a7ec9a0db8c8948b1eb91906285ddd
Sathya Sai <sathyasai410@gmail.com>
2026-03-16 21:40:14 +0530

    fixed

--- src/components/UnicornBackground.jsx
+++ src/components/UnicornBackground.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import UnicornScene from "unicornstudio-react";
 
 export default function UnicornBackground() {
@@ -10,17 +10,5 @@ export default function UnicornBackground() {
           
     </div>
   );
-=======
-import UnicornScene from "unicornstudio-react";
 
-export default function UnicornBackground() {
-  return (
-    <div className="unicorn-bg">
-      <UnicornScene projectId="0iyaWU2vXPuPkwMYV4mw"
-      width="100%"
-          height="100%" />
-          
-    </div>
-  );
->>>>>>> f28f9da7251204656258f8394977c7a1fc1d019a
 }
